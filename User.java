979dcc1a98265ae7360e14d37e8887746f58516c
evenler <715350825@qq.com>
2024-09-02 15:27:19 +0800

    ClassLoaderTest

---
+++ User.java
@@ -0,0 +1,34 @@
+public class User {
+
+    private int id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sout() {
+        System.out.println("=======自己的加载器加载类调用方法=======");
+    }
+}
